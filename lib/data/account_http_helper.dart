45c3d0e9a5da9a90f4238abbf2c63bd363c6e5c5
MaikKBRNK <maikkebernik@gmail.com>
2022-11-02 19:46:18 +0100

    Small fix

--- lib/data/account_http_helper.dart
+++ lib/data/account_http_helper.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:learning_analytics/data/account.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:learning_analytics/data/http_helper.dart';
 
 class AccountHttpHelper {
   final String authority = 'api.learning-analytics.gang-of-fork.de';
@@ -48,8 +49,11 @@ class AccountHttpHelper {
     };
 
     http.Response response = await http.post(uri, headers: headers, body: body);
-    var login = json.decode(response.body)["token"]["token"];
-    await prefs.setString("jwt", login);
+    var loginToken = json.decode(response.body)["token"]["token"];
+    await prefs.setString("jwt", loginToken);
+
+    var role = (await HttpHelper().getUser(loginToken)).role;
+    await prefs.setString("role", role);
 
     if (response.statusCode == 201) {
       return true;
